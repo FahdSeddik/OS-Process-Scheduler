cb136ef21e2077904a4c0d27b688ae40968944b4
Fahd Seddik <62207434+FahdSeddik@users.noreply.github.com>
2024-04-23 20:00:03 +0200

    Pseudo Code for process generator

--- Phase 1 (Scheduler)/code/process_generator.c
+++ Phase 1 (Scheduler)/code/process_generator.c
@@ -18,6 +18,19 @@ int main(int argc, char * argv[])
     // 5. Create a data structure for processes and provide it with its parameters.
     // 6. Send the information to the scheduler at the appropriate time.
     // 7. Clear clock resources
+
+    // Pseudo code: (could be modified)
+    // 1. Ask user which algorithm do they want using a function (HPF, SRTN, RR with quantum)
+    // 2. Read Input file using a function
+    // 3. Create messageQueue using mqCreate() // sync with who wrote the scheduler to know params
+    // 4. Create a semaphore to be used in synchronization using semCreate() // sync with who wrote the scheduler to know params
+    // 5. Initiate scheduler using pmRunProcess sending it params read from user input (algorithm, quantum (if applicable))
+    // 6. Call program loop function that would do the following:
+    //      1. Have a while loop with a condition that it still didnt finish all processes
+    //      2. Should down the semaphore to ensure that if you have something to send in message queue, the scheduler will wait for it (scheduler has up)
+    //      3. Should send *all* processes that arrive at the current clock using the message queue mqSend()
+    //      4. Should wait until next clock using one-line busy wait while(time == getClk());
+
     destroyClk(true);
 }
 
